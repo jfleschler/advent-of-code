747a7584208d1720f28aad399d87f7877a82373f
Andre Azzolini <apazzolini@gmail.com>
2018-01-30 00:27:44 -0600

    Update configs and template

--- templates/test.js
+++ templates/test.js
@@ -1,16 +1,18 @@
 import { solvePart1, solvePart2 } from './dayXXX.js'
 
+const example = ``
+
 describe('day XXX', () => {
     describe('part 1', () => {
-        test('', () => {
-            const answer = solvePart1()
+        test('solves an example', () => {
+            const answer = solvePart1(example)
             expect(answer).toEqual()
         })
     })
 
     describe('part 2', () => {
-        test('', () => {
-            const answer = solvePart2()
+        test('solves an example', () => {
+            const answer = solvePart2(example)
             expect(answer).toEqual()
         })
     })
